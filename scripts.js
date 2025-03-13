d0a09f8b3c50ddfeea9477dd071b20e130d04677
YeimiGuido <yeimi.guido@est.ulsa.edu.ni>
2025-03-13 13:47:11 -0600

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+//Reto 1: Saludo Personalizado
+ let nombre=prompt("ingrese su nombre");
+
+ function saludar(nombre){
+ 	console.log("¡Hola "+ nombre + ", bienvenido/a!");
+  let saludo= "¡Hola "+ nombre + ", bienvenido/a!";
+  document.getElementById("resultado").innerHTML=saludo;
+ }
+ saludar(nombre);
+ 
+ //Reto 2: Conversor de Grados Celsius a Fahrenheit
+ function convercion(celcius,fahrenheit){
+ 	
+ }
